e96b0721096067e3b29edf96d0c95e12a86334e4
Alex-Dylevskaa <115581162+Alex-Dylevskaa@users.noreply.github.com>
2022-11-07 03:42:00 +0300

    Add files via upload

---
+++ javascript/assets/js/maim.js
@@ -0,0 +1,87 @@
+//Задачи для решения
+//1
+// let element = ['Привет, ', 'мир', '!'];
+// alert(element[0] + element[1] + element[2]);
+//
+//2
+// let element = ['Привет, ', 'мир', '!'];
+// let text = element[0] + element[1] + element[2]; 
+// alert(text);
+//
+// 3
+// let element = ['Привет, ', 'мир!'];
+// element[0] = 'Пока';
+// alert(element);
+//
+//5
+// let element = {
+// 	'ru':['голубой', 'красный', 'зеленый'],
+// 	'en':['blue', 'red', 'green'],
+// };
+// alert(element['ru'][1]);
+//
+//
+//Работа с массивами
+//
+//1
+// let arr = ['a', 'b', 'c'];
+// alert(arr[0] + arr[1] + arr[2]);
+//
+//2
+// let arr = ['a', 'b', 'c'];
+// alert(arr[0]);
+// alert(arr[1]);
+// alert(arr[2]);
+//
+//3
+// let arr = ['a', 'b', 'c', 'd'];
+// let p = '+';
+// alert (arr[0] + p + arr[1]);
+// alert (arr[2] + p + arr[3]);
+//
+//4
+// let arr = ['2', '5', '3', '9'];
+// let a = (arr[0] * arr[1]);
+// let b = (arr[2] * arr[3]);
+// let result = (a + b);
+// alert (result)
+//
+//
+//Объекты (ассоциативные массивы)
+//1.1
+// let obj = {a: 1, b: 2, c: 3};
+// alert(obj['c']);
+//
+//2
+// let obj = {'Коля':'1000', 'Вася':'500', 'Петя':'200'};
+// alert(obj['Петя']); 
+// alert(obj['Коля']);
+//
+//3
+// let day = {1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'};
+// alert(day['5']);
+//
+//
+//Многомерные массивы
+//1
+// let arr = {
+//     'oo': [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ],
+// }
+// alert(arr['oo'][1][0]);
+//
+//2
+// let arr = {
+//     js: ['jQuery', 'Angular'],
+//     php: ['hello'],
+//     css: ['world'],
+// }
+// alert(arr['js'] [0]);
+//
+//3
+// let arr = {
+//     'ru': ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье',],
+//     'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', ],
+// }
+// alert(arr['ru'][0]);
+// alert(arr['en'][2]);
+//
